eab4f7db451c0ee741436fc4f1ab4d25e3f6c736
Ben <blideside16@gmail.com>
2024-10-16 14:50:19 -0400

    Fix import path for Component.module.css

--- src/Component.js
+++ src/Component.js
@@ -4,7 +4,7 @@ import { useState, useEffect, useRef } from 'react';
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { ScrollArea } from "./components/ui/scroll-area";
-import styles from "./Component.module.css"; // Import the CSS module
+import styles from './components/Component.module.css';
 
 const Sprite = ({ x, y, size, color }) => (
   <div
