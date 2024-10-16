6b5841b3bb5423070c73ceeb70ef4b9f8f527678
Ben <blideside16@gmail.com>
2024-10-16 14:14:44 -0400

    Remove unused AdminDashboard import from Component.js

--- src/Component.js
+++ src/Component.js
@@ -4,7 +4,6 @@ import { useState, useEffect, useRef } from 'react';
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { ScrollArea } from "./components/ui/scroll-area";
-import AdminDashboard from "./components/AdminDashboard";
 
 const Sprite = ({ x, y, size, color }) => (
   <div
